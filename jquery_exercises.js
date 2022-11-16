11ef948ce53360f5cbae2eb5ff35aace84cab8ad
Matias Silva <silvamatias03@gmail.com>
2022-11-16 10:56:11 -0600

    jquery exercise 2(p3)

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -12,7 +12,15 @@ $(function() {
         // alert(h1Contents);
 
         //Second Exercise(p2)
-        $('.codeup').css('border', '1px solid red');
+        // $('.codeup').css('border', '1px solid red');
+
+        //Second Exercise(p3)
+        $('li').css('font-size', '20px');
+        $('h1').css('background-color', 'yellow');
+        $('p').css('background-color', 'yellow');
+        $('li').css('background-color', 'yellow');
+        let h1Contents = $('h1').html();
+        alert(h1Contents);
     })
 
 })
