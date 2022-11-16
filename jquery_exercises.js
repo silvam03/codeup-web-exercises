018af2841b796a206b5c295fb40ff17a85fcb934
Matias Silva <silvamatias03@gmail.com>
2022-11-16 11:21:51 -0600

    Completed jQuery Exercise 2 (p4/4)

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,26 +1,29 @@
 "use strict";
-$(function() {
+(function() {
     $(document).ready(function() {
         //First Exercise
         // alert("DOM has finished loading!");
 
-        //Second Exercise(p1)
+        //Second Exercise(p1-Id Selectors)
         // let divContents = $('#player-1').html();
         // alert(divContents);
         //
         // let h1Contents = $('#player-1-name').html();
         // alert(h1Contents);
 
-        //Second Exercise(p2)
+        //Second Exercise(p2-Class Selectors)
         // $('.codeup').css('border', '1px solid red');
 
-        //Second Exercise(p3)
-        $('li').css('font-size', '20px');
-        $('h1').css('background-color', 'yellow');
-        $('p').css('background-color', 'yellow');
-        $('li').css('background-color', 'yellow');
-        let h1Contents = $('h1').html();
-        alert(h1Contents);
+        //Second Exercise(p3-Element Selectors)
+        // $('li').css('font-size', '20px');
+        // $('h1').css('background-color', 'yellow');
+        // $('p').css('background-color', 'yellow');
+        // $('li').css('background-color', 'yellow');
+        // let h1Contents = $('h1').html();
+        // alert(h1Contents);
+
+        //Second Exercise(p3-Multiple Selectors)
+        $('h1, p, li').css('background-color', 'yellow');
     })
 
-})
+})();
