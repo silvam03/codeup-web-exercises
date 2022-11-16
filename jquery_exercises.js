1360b95082b166bb2431271b2b6a4b973b7dd9b7
Matias Silva <silvamatias03@gmail.com>
2022-11-16 10:48:39 -0600

    jquery exercise 2(p2)

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,6 +1,18 @@
 "use strict";
 $(function() {
     $(document).ready(function() {
-        alert("DOM has finished loading!")
+        //First Exercise
+        // alert("DOM has finished loading!");
+
+        //Second Exercise(p1)
+        // let divContents = $('#player-1').html();
+        // alert(divContents);
+        //
+        // let h1Contents = $('#player-1-name').html();
+        // alert(h1Contents);
+
+        //Second Exercise(p2)
+        $('.codeup').css('border', '1px solid red');
     })
+
 })
